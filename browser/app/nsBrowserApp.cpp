a90bd355855d3b86cbb75cf43f8dffadbdcf4a9b
Nicole LeGare <legaren@immunant.com>
2025-04-08 09:28:01 -0700

    Copy __wrap_main into nsBrowserApp.cpp and also use dlsym for loading ia2_stackptr_1

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -6,13 +6,119 @@
 extern "C" {
   #include <ia2.h>
 
-  INIT_RUNTIME(1);
+//   INIT_RUNTIME(1);
 
-  // This must be defined before including the following line
-  #define IA2_COMPARTMENT 1
+//   // This must be defined before including the following line
+//   #define IA2_COMPARTMENT 1
 
-  #include <ia2_compartment_init.inc>
+//   #include <ia2_compartment_init.inc>
+
+#include <dlfcn.h>
+
+void *ia2_get_stackptr_1(void) {
+    void *handle = dlopen("libxul.so", RTLD_NOW | RTLD_GLOBAL);
+    assert(handle);
+    void *res = dlsym(handle, "ia2_stackptr_1");
 }
+
+#if 0
+  /* Stores the stack pointer to return to after main() is called. */
+  static void *main_sp __attribute__((used)) = 0;
+
+  /* XXX: Assumes main compartment has pkey 1. */
+  /* clang-format off */
+  int __wrap_main(int argc, char **argv);
+  __asm__(
+      ".global __wrap_main\n"
+      "__wrap_main:\n"
+  #if defined(__x86_64__)
+      "pushq %rbp\n"
+      "movq %rsp, %rbp\n"
+      // Switch pkey to the appropriate compartment.
+      "xor %ecx,%ecx\n"
+      "mov %ecx,%edx\n"
+      "mov_pkru_eax 1\n"
+      "wrpkru\n"
+      // Save the old stack pointer in main_sp.
+      "movq %rsp, main_sp(%rip)\n"
+
+      "call ia2_get_stackptr_1\n"
+      "movq %rax, %rsp\n"
+
+      // Load the stack pointer for this compartment's stack.
+      // "mov ia2_stackptr_1@GOTTPOFF(%rip), %r11\n"
+      // "mov %fs:(%r11), %rsp\n"
+
+      // Align the stack before calling main.
+      "subq $8, %rsp\n"
+      // Call the real main function.
+      "call __real_main\n"
+      // Restore the old stack pointer before returning.
+      "mov main_sp(%rip), %rsp\n"
+      // Save return value
+      "mov %rax,%r10\n"
+      // Switch pkey to untrusted compartment
+      "xor %ecx,%ecx\n"
+      "xor %edx,%edx\n"
+      "mov_pkru_eax 0\n"
+      "wrpkru\n"
+      // Restore return value
+      "mov %r10,%rax\n"
+      "popq %rbp\n"
+      "ret\n"
+  #elif defined(__aarch64__)
+      // prologue
+      "stp x29, x30, [sp, #-16]!\n"
+      "mov x29, sp\n"
+
+      // Save old stack pointer in main_sp
+      "adrp x9, main_sp\n"
+      "add x9, x9, #:lo12:main_sp\n"
+      // Tag x9 with compartment 1
+      "orr x9, x9, #0x100000000000000\n"
+
+      "str x29, [x9]\n"
+
+      // Load the new stack pointer
+      // Since this accesses a TLS in the same DSO it's simpler than the TLS reference in ia2_internal.h
+      "mrs x9, tpidr_el0\n"
+      "add x9, x9, #:tprel_hi12:ia2_stackptr_1\n"
+      "add x9, x9, #:tprel_lo12_nc:ia2_stackptr_1\n"
+
+      // Tag x9 with compartment 1
+      "orr x9, x9, #0x100000000000000\n"
+
+      "ldr x9, [x9]\n"
+      "mov sp, x9\n"
+
+      // Set x18 tag to 1
+      "movz_shifted_tag_x18 1\n"
+
+      // Call the real main function
+      "bl __real_main\n"
+
+      // Set x18 tag to 0
+      "movz_shifted_tag_x18 0\n"
+
+      // Restore the old stack pointer
+      "adrp x9, main_sp\n"
+      "add x9, x9, #:lo12:main_sp\n"
+
+      // Tag x9 with compartment 1
+      "orr x9, x9, #0x100000000000000\n"
+
+      "ldr x9, [x9]\n"
+      "mov sp, x9\n"
+
+      "ldp x29, x30, [sp], #16\n"
+      "ret"
+  #endif
+  );
+
+  __attribute((tls_model("initial-exec"))) __attribute__((__unused__)) __attribute__((visibility ("default"))) extern __thread void *ia2_stackptr_1;
+#endif
+}
+
 
 #include "nsXULAppAPI.h"
 #include "mozilla/XREAppData.h"
